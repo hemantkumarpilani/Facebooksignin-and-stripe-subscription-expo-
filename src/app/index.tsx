352c4aa917e32177b27e214432c95e8934645d18
Hemant Kumar <hemant@infotech.works>
2025-12-04 10:53:29 +0530

    added dummy keys in the index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,16 @@ import { Redirect } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
 
 // Gatekeeper screen: decides where to send the user based on auth state
+// In backend -> .env file
+// STRIPE_SECRET_KEY =
+//   sk_test122321312232;
+
+// In root -> .env file
+// EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY =
+//   pk_test_12345;
+// EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY =
+//   pk_test_23424345;
+
 export default function Index() {
   const { isLoaded, isSignedIn } = useAuth();
 
